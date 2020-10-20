2788d36b5a6fd823e9992885c149df25399c1ac1
Tej Chavan <tejasvini.chavan@phoenix.edu>
2020-10-20 02:37:40 +0000

    corrected typo of tag

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    department = "devops"
+    Department = "devops"
     Billable = "true"
   }
 }
